e464b8d7babb336126339a976130eec429f2345e
Alexander Leinoff <alexander-leinoff@uiowa.edu>
2016-05-20 15:42:23 -0500

    ENH: BRAINSDeface now reads in an iamge, an atlas and a landmarks file

--- BRAINSDeface/BRAINSDeface.cxx
+++ BRAINSDeface/BRAINSDeface.cxx
@@ -2,11 +2,33 @@
 // Created by Leinoff, Alexander on 5/20/16.
 //
 
-//#include <BRAINSCommonLib.h>
+
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
 #include <BRAINSDefaceCLP.h>
+#include <Slicer3LandmarkIO.h>
+#include <itkBinaryThresholdImageFilter.h>
+
 
 int main(int argc, char **argv)
 {
-  std::cout << "Hello World!!" << std::endl;
+  PARSE_ARGS;
+
+  //Read in the imagefile. Right now assumed to be t1 weighted image
+  typedef itk::Image<double,3> ImageType;
+  typedef itk::ImageFileReader<ImageType> ImageReaderType;
+  ImageReaderType::Pointer imageReader = ImageReaderType::New();
+  imageReader->SetFileName(inputImage);
+
+  //Read in the atlas label file
+  typedef itk::Image<double,3> LabelAtlasType;
+  typedef itk::ImageFileReader<LabelAtlasType> LabelAtlasReaderType;
+  LabelAtlasReaderType::Pointer labelAtlasReader = LabelAtlasReaderType::New();
+  labelAtlasReader->SetFileName(labelmap);
+
+  //Read in the landmarks file
+  LandmarksMapType myLandmarks = ReadSlicer3toITKLmk(landmarks);
+
+
   return EXIT_SUCCESS;
 }
