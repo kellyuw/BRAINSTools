e6c5e78772762cd08528b04e20da8cbbff807156
Jeff Obadal <jeffrey-obadal@uiowa.edu>
2016-06-14 09:56:13 -0500

    WIP: BRAINSDeface added template for BRAINSDeface Test Brain Data
    Copied the testing template into the TestSuite directory, and renamed appropriate variables.

---
+++ BRAINSDeface/TestSuite/BRAINSDefaceBrainDataTest.cxx
@@ -0,0 +1,16 @@
+//
+// Created by
+//
+
+#include <iostream>
+#include "BRAINSDefaceBrainDataTestCLP.h"
+
+
+int main(int argc, char* argv[])
+{
+  PARSE_ARGS;
+
+  std::cout << "First BRAINSDeface Test!!!" << std::endl;
+
+  return EXIT_SUCCESS;
+}
